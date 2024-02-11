412b91011cb8bec350343ed4438241f33fff043c
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-11 19:29:22 +0500

    added #7 Add to cart

--- src/app/(.)product/[id]/page.tsx
+++ src/app/(.)product/[id]/page.tsx
@@ -7,7 +7,8 @@ import CustomImage from "@/components/image";
 import { useParams, useRouter } from "next/navigation";
 import { StarIcon as StarIconOutline } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/24/solid";
-import ReactStars from 'react-stars'
+import ReactStars from "react-stars";
+import { toast } from "react-toastify";
 
 const ProductDetailedPage = () => {
   const [laoding, setLaoding] = useState(false);
@@ -16,6 +17,29 @@ const ProductDetailedPage = () => {
 
   const { id } = useParams();
   const router = useRouter();
+
+  const handleClick = () => {
+    const products: ProductType[] =
+      JSON.parse(localStorage.getItem("carts") as string) || [];
+    const isExistProduct = products.find((c) => c.id === product?.id);
+
+    if (isExistProduct) {
+      const updatedData = products.map((c) => {
+        if (c.id === product?.id) {
+          return {
+            ...c,
+            quantity: c.quantity + 1,
+          };
+        }
+        return c;
+      });
+      localStorage.setItem("carts", JSON.stringify(updatedData));
+    } else {
+      const data = [...products, { ...product, quantity: 1 }];
+      localStorage.setItem("carts", JSON.stringify(data));
+    }
+    toast("Product added to your bag!!");
+  };
 
   useEffect(() => {
     async function getData() {
@@ -79,7 +103,10 @@ const ProductDetailedPage = () => {
                               />
                             )
                           )} */}
-                          <ReactStars value={product?.rating?.rate} edit={false} />
+                          <ReactStars
+                            value={product?.rating?.rate}
+                            edit={false}
+                          />
                         </div>
                       )}
                       <p className="text-blue-600 hover:underline cursor-pointer text-xs">
@@ -91,7 +118,10 @@ const ProductDetailedPage = () => {
                     </p>
                   </div>
                   <div className="space-y-3 text-sm">
-                    <button className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
+                    <button
+                      className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black"
+                      onClick={handleClick}
+                    >
                       Add to bag
                     </button>
                     <button
---
+++ src/app/shopping-cart/page.tsx
@@ -0,0 +1,6 @@
+
+const ShoppingCart = () => {
+  return <div>ShoppingCart</div>;
+};
+
+export default ShoppingCart;
