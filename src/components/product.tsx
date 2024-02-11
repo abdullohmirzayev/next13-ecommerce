91e3cc87c2dd6ed4902b944a67f2331baa3e387c
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-11 09:58:35 +0500

    added #3 Image optimization

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,21 +1,23 @@
 'use client'
 
 import { ProductType } from "@/interface"
+import Link from "next/link"
 import { FC } from "react"
+import CustomImage from "./image"
 
 const Product: FC<{ product: ProductType }> = ({ product }) => {
     return (
-        <div>
-            <div className="bg-gray-100 p-6 rounded-lg">
-                <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
-                <div className="font-semibold flex items-center justify-between mt-4 mb-1">
-                    <p className="w-44 truncate">{product.title}</p>
-                    <p>{product.price}$</p>
-                </div>
-                <p className="leading-relaxed text-base line-clamp-2">{product.description}</p>
+        <Link href={`/product/${product.id}`} className="h-96 flex flex-col p-6 rounded-lg group hover:scale-105 transition-transform ease-out duration-200 border">
+            <div className="relative max-h-80 flex-1">
+                <CustomImage product={product} fill />
             </div>
-        </div>
+            <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{product.category}</h3>
+            <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+                <p className="w-44 truncate">{product.title}</p>
+                <p>{product.price}$</p>
+            </div>
+            <p className="leading-relaxed text-base line-clamp-2">{product.description}</p>
+        </Link>
     )
 }
 
