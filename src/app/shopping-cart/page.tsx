9f4329ed4c0395c3687cb23b9b9d73fe0eadcd14
Abdulloh <abdullohmirzayev7570@gmail.com>
2024-02-12 12:28:57 +0500

    added #10 Total price & empty cart

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -3,9 +3,11 @@
 import ReactStars from "react-stars";
 import { ProductType } from "@/interface";
 import CustomImage from "@/components/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const ShoppingCart = () => {
+  const [total, setTotal] = useState<number>(0);
   const [products, setProducts] = useState<ProductType[]>(
     JSON.parse(localStorage.getItem("carts") as string) || []
   );
@@ -50,117 +52,200 @@ const ShoppingCart = () => {
     }
   };
 
+  useEffect(() => {
+    const total = products.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0);
+    setTotal(total);
+  }, [products]);
+
   return (
-    <div className="h-screen bg-gray-100 pt-20">
-      <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
-      <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-        <div className="rounded-lg md:w-2/3">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start"
-            >
-              <div className="relative w-52">
-                <CustomImage product={product} fill />
-              </div>
-              <div className="sm:ml-4 sm:flex sm:w-full gap-x-4 sm:justify-between">
-                <div className="mt-5 sm:mt-0">
-                  <h2 className="text-lg font-bold text-gray-900 line-clamp-1">
-                    {product.title}
-                  </h2>
-                  <p className="mt-1 text-xs text-gray-700 line-clamp-2">
-                    {product.description}
-                  </p>
-                  <div className="flex items-center text-sm my-4">
-                    <p>{product?.rating.rate}</p>
-                    {product?.rating?.rate && (
-                      <div className="flex items-center ml-2 mr-6">
-                        <ReactStars
-                          value={product?.rating?.rate}
-                          edit={false}
-                        />
+    <>
+      {products.length ? (
+        <div className="h-screen bg-gray-100 pt-20">
+          <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+          <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+            <div className="rounded-lg md:w-2/3">
+              {products.map((product) => (
+                <div
+                  key={product.id}
+                  className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start"
+                >
+                  <div className="relative w-52">
+                    <CustomImage product={product} fill />
+                  </div>
+                  <div className="sm:ml-4 sm:flex sm:w-full gap-x-4 sm:justify-between">
+                    <div className="mt-5 sm:mt-0">
+                      <h2 className="text-lg font-bold text-gray-900 line-clamp-1">
+                        {product.title}
+                      </h2>
+                      <p className="mt-1 text-xs text-gray-700 line-clamp-2">
+                        {product.description}
+                      </p>
+                      <div className="flex items-center text-sm my-4">
+                        <p>{product?.rating.rate}</p>
+                        {product?.rating?.rate && (
+                          <div className="flex items-center ml-2 mr-6">
+                            <ReactStars
+                              value={product?.rating?.rate}
+                              edit={false}
+                            />
+                          </div>
+                        )}
+                        <p className="text-blue-600 hover:underline cursor-pointer text-xs">
+                          See all {product?.rating?.count} reviews
+                        </p>
                       </div>
-                    )}
-                    <p className="text-blue-600 hover:underline cursor-pointer text-xs">
-                      See all {product?.rating?.count} reviews
-                    </p>
+                    </div>
+                    <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                      <div className="flex items-center border-gray-100">
+                        <span
+                          className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                          onClick={() => handleDecrement(product.id)}
+                        >
+                          {" "}
+                          -{" "}
+                        </span>
+                        <input
+                          className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                          type="number"
+                          value={product.quantity}
+                          min="1"
+                          readOnly
+                        />
+                        <span
+                          className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
+                          onClick={() => handleIncrement(product.id)}
+                        >
+                          {" "}
+                          +{" "}
+                        </span>
+                      </div>
+                      <div className="flex items-center space-x-4">
+                        <p className="text-sm">
+                          {(product.price * product.quantity).toLocaleString(
+                            "en-US",
+                            {
+                              style: "currency",
+                              currency: "usd",
+                            }
+                          )}
+                        </p>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                          onClick={() => removeProduct(product.id)}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                  <div className="flex items-center border-gray-100">
-                    <span
-                      className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
-                      onClick={() => handleDecrement(product.id)}
-                    >
-                      {" "}
-                      -{" "}
-                    </span>
-                    <input
-                      className="h-8 w-8 border bg-white text-center text-xs outline-none"
-                      type="number"
-                      value={product.quantity}
-                      min="1"
-                      readOnly
-                    />
-                    <span
-                      className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
-                      onClick={() => handleIncrement(product.id)}
-                    >
-                      {" "}
-                      +{" "}
-                    </span>
-                  </div>
-                  <div className="flex items-center space-x-4">
-                    <p className="text-sm">
-                      {product.price.toLocaleString("en-US", {
-                        style: "currency",
-                        currency: "usd",
-                      })}
-                    </p>
+              ))}
+            </div>
+            <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+              <div className="mb-2 flex justify-between">
+                <p className="text-gray-700">Subtotal</p>
+                <p className="text-gray-700">
+                  {total.toLocaleString("en-US", {
+                    currency: "usd",
+                    style: "currency",
+                  })}
+                </p>
+              </div>
+              <div className="flex justify-between">
+                <p className="text-gray-700">Shipping</p>
+                <p className="text-gray-700">
+                  {(10).toLocaleString("en-US", {
+                    currency: "usd",
+                    style: "currency",
+                  })}
+                </p>
+              </div>
+              <hr className="my-4" />
+              <div className="flex justify-between">
+                <p className="text-lg font-bold">Total</p>
+                <div className="">
+                  <p className="mb-1 text-lg font-bold">
+                    {(total + 10).toLocaleString("en-US", {
+                      currency: "usd",
+                      style: "currency",
+                    })}
+                  </p>
+                  <p className="text-sm text-gray-700">including VAT</p>
+                </div>
+              </div>
+              <button className="mt-6 w-full rounded-md bg-blue-500 py-4 font-medium text-blue-50 hover:bg-blue-600">
+                Check out
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <section className="bg-white ">
+          <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+            <div className="wf-ull lg:w-1/2">
+              <p className="text-sm font-medium text-blue-700 dark:text-blue-600">
+                404 error
+              </p>
+              <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-black md:text-3xl">
+                Shopping cart is empty
+              </h1>
+              <p className="mt-4 text-gray-500 dark:text-gray-500">
+                Sorry, the page you are looking for doesn't exist.Here are some
+                helpful links:
+              </p>
+
+              <div className="flex items-center mt-6 gap-x-3">
+                <Link href={"/"}>
+                  <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      strokeWidth="1.5"
+                      stroke-width="1.5"
                       stroke="currentColor"
-                      className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
-                      onClick={() => removeProduct(product.id)}
+                      className="w-5 h-5 rtl:rotate-180"
                     >
                       <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 18L18 6M6 6l12 12"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
                       />
                     </svg>
-                  </div>
-                </div>
+
+                    <span>Home</span>
+                  </button>
+                </Link>
+
+                <Link href={"/products"}>
+                  <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                    Products
+                  </button>
+                </Link>
               </div>
             </div>
-          ))}
-        </div>
-        <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-          <div className="mb-2 flex justify-between">
-            <p className="text-gray-700">Subtotal</p>
-            <p className="text-gray-700">$129.99</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="text-gray-700">Shipping</p>
-            <p className="text-gray-700">$4.99</p>
-          </div>
-          <hr className="my-4" />
-          <div className="flex justify-between">
-            <p className="text-lg font-bold">Total</p>
-            <div className="">
-              <p className="mb-1 text-lg font-bold">$134.98 USD</p>
-              <p className="text-sm text-gray-700">including VAT</p>
+
+            <div className="relative w-full mt-8 lg:w-1/2 lg:mt-0">
+              <img
+                className=" w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover "
+                src="https://images.unsplash.com/photo-1613310023042-ad79320c00ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                alt=""
+              />
             </div>
           </div>
-          <button className="mt-6 w-full rounded-md bg-blue-500 py-4 font-medium text-blue-50 hover:bg-blue-600">
-            Check out
-          </button>
-        </div>
-      </div>
-    </div>
+        </section>
+      )}
+    </>
   );
 };
 
